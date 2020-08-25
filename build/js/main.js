64864ec3679db83a89d62e2fbcec5d72787236fc
mazax1ct <vova@giveandget.ru>
2020-08-25 18:25:15 +0300

    typo and popups

--- build/js/main.js
+++ build/js/main.js
@@ -1,3 +1,18 @@
+//мобильное меню
+$(document).on('click', '.js-mobile-menu-opener', function () {
+  $('body').addClass('mobile-menu-open');
+  $('.mobile-menu').addClass('is-open');
+  return false;
+});
+
+//закрытие мобильное меню
+$(document).on('click', '.js-mobile-menu-closer', function () {
+  $('body').removeClass('mobile-menu-open');
+  $('.mobile-menu').removeClass('is-open');
+  return false;
+});
+
+//каталог и материалы
 $(document).on('click', '.js-menu-opener', function () {
   if($(this).hasClass('is-active')) {
     $(this).removeClass('is-active');
@@ -16,10 +31,17 @@ $(document).on('click', '.js-menu-opener', function () {
   return false;
 });
 
+//закрытие каталог и материалы
 $(document).on('click', '.js-menu-closer', function () {
   $('body').removeClass('menu-open');
   $('.header__dropdown').removeClass('is-open');
   $('.js-menu-opener.is-active').find('use').attr('xlink:href', '#burger');
   $('.js-menu-opener.is-active').removeClass('is-active');
+  return false;
+});
+
+//закрытие попапа
+$(document).on('click', '.js-popup-close', function () {
+  $.fancybox.close();
   return false;
 });
